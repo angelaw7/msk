814d868100263bbb399dbe942cb4b114291ca200
Angela Wang <wangela472@gmail.com>
2022-06-23 00:13:00 -0400

    Add more comments

--- main.go
+++ main.go
@@ -15,25 +15,30 @@ import (
 )
 
 func main() {
+	// Loads the .env file
 	godotenv.Load()
 
+	// Setup
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 
+	// Connect to database
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Use the planets collection (db.sample_guides.planets)
 	planetsCollection := client.Database("sample_guides").Collection("planets")
-	id, _ := primitive.ObjectIDFromHex("621ff30d2a3e781873fcb621")
+	id, _ := primitive.ObjectIDFromHex("621ff30d2a3e781873fcb677")
 	opts := options.Update().SetUpsert(true)
 
+	// Call the collection.UpdateOne() method
 	result, err := planetsCollection.UpdateOne(
 		ctx,
-		bson.M{"_id": id},
+		bson.M{"_id": id, "note": "yay"},
 		bson.D{
-			{"$set", bson.D{{"name", "Uranus + owo"}}},
+			{"$set", bson.D{{"name", "Pluto + owo"}}},
 		},
 		opts,
 	)
@@ -42,6 +47,7 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Print statements depending on what action was performed
 	if result.ModifiedCount == 1 {
 		fmt.Println("Updated 1 Documents")
 	} else if result.MatchedCount == 0 {
